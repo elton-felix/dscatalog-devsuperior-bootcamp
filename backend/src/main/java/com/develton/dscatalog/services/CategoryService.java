a60cb64db7f1e80acd526b72db277ee79cd786e9
EltonFelix <eltomfelix@gmail.com>
2022-02-02 19:33:30 -0300

    Database seeding

--- backend/src/main/java/com/develton/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/develton/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.develton.dscatalog.entities.Category;
 import com.develton.dscatalog.repositories.CategoryRepository;
@@ -16,6 +17,8 @@ public class CategoryService {
 	@Autowired //tag faz com que o spring gerencie uma dependencia valida do categoryrepository
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true) //define que esse metodo esteja envolvido numa transação com o banco, para garantirmos a integridade da transação
+	//readOnly=true melhora a performace do BD , para que ele não fique travado só pra leitura
 	public List<Category> findAll(){
 		return repository.findAll();
 	}

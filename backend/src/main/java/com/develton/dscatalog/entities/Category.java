c20485399cdabdf5a1c415b4cca483201979338c
EltonFelix <eltomfelix@gmail.com>
2022-02-08 20:52:16 -0300

    Auditing data

--- backend/src/main/java/com/develton/dscatalog/entities/Category.java
+++ backend/src/main/java/com/develton/dscatalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.develton.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity //dizendo que essa classe é uma entidade
@@ -24,6 +28,12 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //dizendo que esse id é auto incrementavel
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") // configurando que esse instante vai ser salvo no banco de dados com o horario UTC por padrão 
+	private Instant createdAt;// este tipo é pra armazenar um unstante
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") 
+	private Instant updatedAt;
 	
 	public Category() {
 		
@@ -49,8 +59,27 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist //quando dermos um save do JPA pela primeira vez pra criar
+	//ele irá salvar o instante em que for criado 
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate//quando for um save pra atualizar 
+	//chama o metodo pra atualizar
+	public void preUpdated() {
+		updatedAt = Instant.now();
+	}
+
 	//hash code é o um metodo padrão do eclipse para comparar objetos
 	@Override
 	public int hashCode() {

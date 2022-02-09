f369ac420416cba29e96081217cc6862674f333f
EltonFelix <eltomfelix@gmail.com>
2022-02-08 21:23:20 -0300

    Pagination

--- backend/src/main/java/com/develton/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/develton/dscatalog/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.develton.dscatalog.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,8 +33,19 @@ public class CategoryResource {
 	//objeto do spring que irá encapsular uma resposta http
 	//colocamos o tipo dos dados do corpo do metodo entre <>
 	@GetMapping //configurando que esse metodo é um webservice / endpoint
-	public ResponseEntity<List<CategoryDTO>> findAll(){
-		List<CategoryDTO> list = service.findAll();
+	public ResponseEntity<Page<CategoryDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		//o of  permite definir os parametros de instanciação
+		//o direction como é string pois na request HTTP(ele mexe só com string) temos que convertir para o tipo enumedado Direction
+		
+		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
 		
 		return ResponseEntity.ok().body(list);//o ok fala que a resposta é 200 que significa que a requisição foi realizada com sucesso
 	}
